412896a17d80e6ce05b436d2cbf81ea32b3af5ea
Carlota <carlota.catot@hp.com>
2020-04-03 00:16:52 +0200

    PastEdition talks youtube videos embeded

--- src/views/PastEdition.vue
+++ src/views/PastEdition.vue
@@ -1,62 +1,40 @@
 <template>
   <div id="challenges" class="under-header padding-bottom">
     <div class="container">
-      <div class="panel large">
-        <h1>HackUPC</h1>
-        <div>
-          <h2 style="text-align: center;">HackUPC prizes</h2>
-          <div class="prizes-container">
-            <div><h3>1st</h3><p>Bose Quietcomfort 35 II</p></div>
-            <div><h3>2nd</h3><p>3D printer Anet A6</p></div>
-            <div><h3>3rd</h3><p>Echo Show 5</p></div>
-          </div>
+      <div class="panel no-margin">
+        <h1>Hackers@UPC</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/m2NDc1ofYXY" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="panel">
-        <h1>McKinsey&Company</h1>
-        <div>
-          <h2 class="panel-info margin-bottom text-centered">Hack the crash</h2>
-          <div class="margin-bottom text-centered">
-            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
-            <span><a target="_blank" rel="noopener" href="https://hackupc2019.slack.com/app_redirect?channel=CNNHM7L8P">#mckinsey</a></span>
-          </div>
-          <p class="margin-bottom">Help us predict damage inflicted in traffic accidents</p>
-          <div>
-            <p><b>Prize: </b>Nintendo switch lite <i>(1 per team member)</i>.</p>
-          </div>
+      <div class="panel no-margin">
+        <h1>Introduction to GIT</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/tNpZOvCiz9o" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="panel">
-        <h1>HP</h1>
-        <div>
-          <h2 class="panel-info margin-bottom text-centered">Talk to 3D Print!</h2>
-          <div class="margin-bottom text-centered">
-            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
-            <span><a target="_blank" rel="noopener" href="https://hackupc2019.slack.com/app_redirect?channel=CN97WQW3V">#hp</a></span>
-          </div>
-          <p class="margin-bottom">Are you able to create a 3D geometry that we can print by voice commands?  Your creativity is welcomed, unleash your imagination to test your skills! Come to our booth A5 1st floor our through Slack</p>
-          <div>
-            <p><b>Prize: </b>4 Soprocket <i>(1 per team member)</i></p>
-          </div>
+      <div class="panel no-margin">
+        <h1>Implementing APIs</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/ofTA5DZUFpk" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="panel">
-        <h1>Facebook</h1>
-        <div>
-          <h2 class="panel-info margin-bottom text-centered">Bring Communities Closer Together</h2>
-          <div class="margin-bottom text-centered">
-            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
-            <span><a target="_blank" rel="noopener" href="https://hackupc2019.slack.com/app_redirect?channel=CNLMVBJKS">#facebook</a></span>
-          </div>
-          <p class="margin-bottom">In Facebook our mission is to bring the world closer together. We build products and applications that build community and would love to see you create something that does the same! It does not need to be a social platform, but just any product that you believe would help bridge people together. Whether it is within your study group, your family, your hometown or the wider world around us, show us something that creates opportunities for people to help and support one another.</p>
-          <div>
-            <p><b>Prizes:</b></p>
-            <ul class="no-indent">
-              <li>1st Prize: Oculus Go <i>(1 per team member)</i></li>
-              <li>2nd Prize: Phillips Bluetooth Headphones <i>(1 per team member)</i></li>
-              <li>3rd Prize: Facebook Branded Backpacks <i>(1 per team member)</i></li>
-            </ul>
-          </div>
+      <div class="panel no-margin">
+        <h1>Introduction to React</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/P3BUEmEB1hg" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="panel no-margin">
+        <h1>Setting up Docker-Compose</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/2aFshEkxMfI" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="panel no-margin">
+        <h1>Carrer paths</h1>
+        <div class="embed-container">
+          <iframe src="https://www.youtube.com/embed/N1-3OEMFSNA" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
     </div>
@@ -71,3 +49,4 @@ export default {
 
 <style scoped>
 </style>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
