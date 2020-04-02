3524dc3ef10be5c996ca184c07761e8f41363514
Carlota <carlota.catot@hp.com>
2020-04-03 00:22:17 +0200

    update FAQs

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,103 +1,65 @@
 <template>
   <div id="faq" class="under-header padding-bottom">
     <div class="container">
-        <div class="panel large">
-          <h1>Travel reimbursement</h1>
-          <div>
-            <p class="margin-bottom">If you have been elected to receive a travel reimbursement, we will contact you after the hackathon to send it to you. You will receive a maximum that has been specified on the email, based on current travelling prices and distances. Requirements to receive travel reimbursement are as follows.</p>
-            <ul class="margin-bottom">
-              <li>Received an email with travel reimbursement amount.</li>
-              <li>Travel reimbursement receipts submitted before the date specified by HackUPC Team.</li>
-              <li>Project submitted to Devpost before due date.</li>
-            </ul>
-            <p>The processing of travel reimbursements takes some time. Our schedule depends on our sponsors' schedule. We expect to have them sent out in a 1-2 months time period. Remember that you need to post a project before the deadline in order to get the reimbursement!</p>
-          </div>
+      <div class="panel large">
+        <h1>What is HackFromHome?</h1>
+        <div>
+          <p>Due to the COVID-19, most of us are confined at home wondering how to spend time at home, while trying to not lose the fight against boredom. For this reason, Hackers@UPC has raised an awesome initiative: an online hackathon! This way, hackathon lovers can spend their time at home doing what they love the most.</p>
         </div>
+      </div>
       <div class="panel">
-        <h1>When shall I arrive?</h1>
+        <h1>Who can participate?</h1>
         <div>
           <p>
-            Registration will start at 4 PM on Friday. The opening ceremony is at 7 PM.
+            If you love hackathons and want to spend an awesome day, this is for you. No need to be a student or have previous knowledge! We encourage you to make great projects! Either related to COVID-19 or not... your imagination is your limit.
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>When is HackUPC ending?</h1>
+        <h1>Where to participate?</h1>
         <div>
           <p>
-            The closing ceremony is expected to end on Sunday 13th at 5:00 PM.
+            You can develop where you feel more comfortable... Your bedroom, the kitchen, the sofa, the bathroom... But please STAY AT HOME! We will soon update you with how to connect with the rest of the community!
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>What if I cannot make it in time?</h1>
+        <h1>I'm not in Discord, how do I get in?</h1>
         <div>
           <p>
-            No problem. Just come to our <a href="#/map/infodesk">InfoDesk</a> for a late check-in. If you are confused, please reach out to any volunteer (white t-shirts) or organizer (black t-shirts).
+            Mail us to <a href="mailto:contact@hackupc.com" target="_blank">contact@hackersatupc.org</a> or join the discord server following <a href="https://discord.gg/AVQHN9p" target="_blank">this link</a>!
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>I'm not in Slack, how do I get in?</h1>
+        <h1>Can I code individually or I have to be in a team?</h1>
         <div>
           <p>
-            Come to the <a href="#/map/infodesk">InfoDesk</a> during the event or send us an email to <a href="mailto:contact@hackupc.com" target="_blank" rel="noopener">contact@hackupc.com</a>
+            You can develop your project alone, with your friend or with someone you meet in on the team building!!
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>Can I leave HackUPC?</h1>
+        <h1>I have no team, what I have to do?</h1>
         <div>
           <p>
-            Yes. However make sure you have your wristband with you, otherwise we might not let you in when you come back.
+            You can find a team in our Team Building activity that will starts at 12:15 (GMT+2) stay tunned! You will find all the information about the activity in the ACTIVITIES INFORMATION tab.
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>Can I sleep now?</h1>
+        <h1>Which will be the language of the Hackathon?</h1>
         <div>
           <p>
-            Yes, sure. You can find the <a href="#/map/sleepingarea">sleeping spaces</a> in A3 and A4 buildings. Some matresses will be provided. Unfortunately, we don't have enough for everyone!
+            English, we are having participants from all over the world so we will be doing all the activities in English.
           </p>
         </div>
       </div>
       <div class="panel">
-        <h1>Something isn’t working!</h1>
+        <h1>Is there any prize?</h1>
         <div>
           <p>
-            If something is not working, please come to our <a href="#/map/infodesk">InfoDesk</a> and meet our awesome volunteers. They'll love to help you!
-          </p>
-        </div>
-      </div>
-      <div class="panel">
-        <h1>Deadline for project submissions?</h1>
-        <div>
-          <p>
-            The deadline for project submissions is Sunday 13th 9:00 AM. Submit your projects <a href="https://hackupc2019.devpost.com">here</a>.
-          </p>
-        </div>
-      </div>
-      <div class="panel">
-        <h1>How does judging work?</h1>
-        <div>
-          <p>
-              We will have a judging expo on Sunday from 10:15 am - 13:15 am. We'll assign you a classroom to present your project (one for HackUPC + as many challenges as you participate), you can check how many hacks there are before yours at <a href="https://my.hackupc.com" target="_blank" rel="noopener">MyHackUPC</a>. The presentation can only be 3 minutes long and with no slides, Prezi's or any visual support except expect your hack. You can check out other projects at any judging room. The top teams will move on to an additional round of panel judging in the Closing Ceremony. If you participate in a challenge, you'll have to present at the specific sponsor room, which is independent of the HackUPC judging.
-          </p>
-        </div>
-      </div>
-      <div class="panel">
-        <h1>Where's my travel reimbursement?</h1>
-        <div>
-          <p>
-            The processing of travel reimbursements takes some time. Our schedule depends on our sponsors' schedule. We expect to have them sent out in a 1 - 2 months time period. Also, remember that you need to post a project before the deadline in order to get the reimbursement.
-          </p>
-        </div>
-      </div>
-      <div class="panel">
-        <h1>I have another question... Biene?</h1>
-        <div>
-          <p>
-            If you have any other questions and you can't find it here, make sure to pay us a visit at our <a href="#/map/infodesk">InfoDesk</a>.
+            Nope! In this hackathon there are no winners we want you to learn, develop, have fun and be distracted! There is no competition, we want to create community.
           </p>
         </div>
       </div>
