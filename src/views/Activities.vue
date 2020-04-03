4279e0f1e22dd413109430ff2ad058becb4d6211
Carlota <carlota.catot@hp.com>
2020-04-03 16:27:34 +0200

    update activities

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,17 +1,115 @@
 <template>
   <div id="activities" class="under-header padding-bottom">
     <div class="container">
-      <div class="panel">
-        <h1>Opening ceremony</h1>
-        <p style="text-align: center;">
-          <a href="https://tv.upc.edu/continguts/hackupc-2019">https://tv.upc.edu/continguts/hackupc-2019</a>
-        </p>
+      <div class="panel large">
+        <h1 style="text-transform: uppercase;">TALKS</h1>
       </div>
       <div class="panel">
-        <h1>Closing ceremony</h1>
-        <p style="text-align: center;">
-          <a href="https://tv.upc.edu/continguts/continguts/cerimonia-cloenda-hackupc">https://tv.upc.edu/continguts/continguts/cerimonia-cloenda-hackupc</a>
-        </p>
+        <h1 style="text-transform: uppercase;">VIM: from zero to hero</h1>
+        <div>
+          <h3>Speaker: Andrea Querol</h3>
+          <h5>Language: Spanish</h5>
+          <p style="text-align: justify;">Aprender a utilizar VIM, uno de los editores texto por consola más populares. Empezando por saber cómo editar, guardar y salir; hasta... bueno, VIM no tiene límites! Este editor se puede encontrar en cualquier Linux y nunca está de más saber manejarse en él.</p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/code.png" class="image" style="width: 30%; margin-top: 2em"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel">
+        <h1 style="text-transform: uppercase;">Cómo crear y entrenar una red neuronal con sólo 10 líneas de Python</h1>
+        <div>
+          <h3>Speaker: Jordi Torres (UPC & BSC)</h3>
+          <h5>Language: Spanish</h5>
+          <p style="text-align: justify;">Esta presentación inicia a los hackers en los conceptos básicos de redes neuronales mediante un caso práctico que con sólo 10 líneas de código Python se crea y entrena una red neuronal que reconoce dígitos escritos a mano. Para ello usaremos la API Keras de TensorFlow, la librería más popular en estos momentos en la comunidad Deep Learning.</p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/ml-icon.png" class="image" style="width: 30%; margin-top: 0.3em"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel">
+        <h1 style="text-transform: uppercase;">Implement a chatbot with Dialogflow</h1>
+        <div>
+          <h3>Speaker: Elena Ruiz</h3>
+          <h5>Language: Spanish</h5>
+          <p style="text-align: justify;">Brew introduction to chatbots. Learn how to implement a chatbot with Dialogflow and how integrate it.</p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/bot.png" class="image" style="width: 30%; margin-top: 1.2em"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel">
+        <h1 style="text-transform: uppercase;">Hacking with Go</h1>
+        <div>
+          <h3>Speaker: Taras Yarema [English]</h3>
+          <h5>Language: English</h5>
+          <p style="text-align: justify;">I like Go. Let's see if I can make you like it too!</p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/go.png" class="image" style="width: 30%; margin-top: 1em"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel large">
+        <h1 style="text-transform: uppercase;">ACTIVITIES</h1>
+      </div>
+      <div class="panel">
+        <h1>TEAM BUILDING</h1>
+        <div>
+          <div class="margin-bottom text-centered">
+            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
+            <span>team-building</span>
+          </div>
+          <p class="margin-bottom">If you are looking a team or additional members for your team, we have an scheduled activity for you! <br /> It will start at 12:15 and it will work the following way: </p>
+          <ul>
+            <li>Join our discord server and go to the text channel #teambuilding.</li>
+            <li>We will ask some questions, you will have to answer at least one!</li>
+            <ul>
+              <li>Any project idea? A webapp, scrapper, a game...</li>
+              <li>Area of interest? Backend, AI, learning a new programming language...</li>
+              <li>Skills? C developer, NodeJS, assembler...</li>
+            </ul>
+            <li>Depending in the amount of people interested, maybe we will suggest you to join an specific voice channel, so you can talk all together and form a team or keep looking for.</li>
+            <li>If you have been in a specific channel, but after all you are still looking for some people/team, come back to the teambuilding channel!</li>
+          </ul>
+        </div>
+      </div>
+      <div class="panel">
+        <h1>ZUMBA</h1>
+        <div>
+          <div class="margin-bottom text-centered">
+            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
+            <span>zumba-class</span>
+          </div>
+          <p class="margin-bottom"> ¡Levántate del sofá y anímate a bailar! Pásatelo en grande siguiendo coreografías latinas que te llenaran de ritmo para hacer ejercicio y disfrutar en esta cuarentena. </p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/zumba.png" class="image" style="width: 100%; margin-top: 3em"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel">
+        <h1>PINTURILLO</h1>
+        <div>
+          <div class="margin-bottom text-centered">
+            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
+            <span>activities</span>
+          </div>
+          <p class="margin-bottom" style="text-align: justify;"> Will you become the next Picasso? Discover it with us! Join our pinturillo battle and have a good time. In case you don’t know what it is, Pinturillo mainly consists of guessing what things are drawing your rivals. Just give it a go, fun is guaranteed! </p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/pen.gif" class="image" style="width: 70%;"/>
+          </div>
+        </div>
+      </div>
+      <div class="panel">
+        <h1>STOP!</h1>
+        <div>
+          <div class="margin-bottom text-centered">
+            <p class="icon"><i class="fa fa-slack" aria-hidden="true"></i></p>
+            <span>activities</span>
+          </div>
+          <p class="margin-bottom" style="text-align: justify;"> Algo per explicar l'activitat </p>
+          <div style="text-align: center;">
+            <img src="/assets/img/live/stop.jpg" class="image" style="width: 80%; margin-top: 1.3em"/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
