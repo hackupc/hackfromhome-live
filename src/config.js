720ee72e9a748138ef8daa34417e51d0750e9cd5
Carlota <carkbra@gmail.com>
2020-04-15 13:31:31 +0200

    updated styles and corrected typos

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import schedule from '../public/data/schedule.json';
 
 export default {
   // If you don't want to test, just comment the fakeTime field
-  // fakeTime: new Date('2020-04-04 20:59:55'), // year, month, day, time
-  notificationTitle: 'HackFromHome Round2',
+  fakeTime: new Date('2020-04-18 20:59:55'), // year, month, day, time
+  notificationTitle: 'HackFromHome Round3',
   notificationIcon: 'favicon.ico',
   baseTimeOffset: -120 * 60,
   hackathon_duration_hours: 12,
