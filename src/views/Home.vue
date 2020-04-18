fa085337720811997730616315c41151ca181d61
Carlota <carkbra@gmail.com>
2020-04-18 11:50:11 +0200

    changed discord server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,7 @@
       <div class="panel" style="align-self: flex-start;">
           <h1>Contacto</h1>
           <div class="text-centered">
-            <span><a  target="_blank" href=" https://discordapp.com/invite/z5eqTGD">Servidor de discord</a></span>
+            <span><a  target="_blank" href="https://discord.gg/kkMZfmt">Servidor de discord</a></span>
           </div>
           <div class="text-centered">
             <p class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></p>
